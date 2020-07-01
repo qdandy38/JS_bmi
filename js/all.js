42680e546db8d80645b279c313b591b46cd6b4f3
YunHsuan Tsai <qdandy38@gmail.com>
2020-07-01 14:12:39 +0800

    first commit

---
+++ js/all.js
@@ -0,0 +1,95 @@
+// DOM
+const send = document.querySelector('.send');
+const height = document.querySelector('.height');
+const weight = document.querySelector('.kg');
+const list = document.querySelector('.list');
+const reset = document.querySelector('.clean');
+
+let data = [];
+send.addEventListener('click', bmiCal, false);
+reset.addEventListener('click', clearAll, false);
+
+function clearAll(e) {
+    e.preventDefault();
+    data = [];
+    render();
+}
+
+function bmiCal(e) {
+    e.preventDefault();
+    if (height.value !== '' && weight.value !== '') {
+        let heightVal = parseInt(height.value.trim());
+        let weightVal = parseInt(weight.value.trim());
+        let bmi = (weightVal / ((heightVal / 100) ** 2)).toFixed(2);
+        let bmiStatus = Status(bmi);
+        const bmiInfo = {
+            height: heightVal,
+            weight: weightVal,
+            bmi: bmi,
+            status: bmiStatus.content,
+            color: bmiStatus.color
+        }
+        data.push(bmiInfo);
+
+        render();
+        height.value = '';
+        weight.value = '';
+    } else {
+        alert('請輸入數值!');
+    }
+}
+
+// 判斷bmi狀態
+function Status(bmi) {
+    let status = {};
+    if (bmi < 18.5) {
+        status = {
+            content: '過輕',
+            color: '#31BAF9'
+        };
+    } else if (bmi >= 18.5 && bmi < 24) {
+        status = {
+            content: '理想',
+            color: '#86D73F'
+        };
+    } else if (bmi >= 24 && bmi < 27) {
+        status = {
+            content: '過重',
+            color: '#FF982D'
+        };
+    } else if (bmi >= 27 && bmi < 30) {
+        status = {
+            content: '輕度肥胖',
+            color: '#FF6C02'
+        };
+    } else if (bmi >= 30 && bmi < 35) {
+        status = {
+            content: '輕度肥胖',
+            color: '#FF6C02'
+        };
+    } else {
+        status = {
+            content: '重度肥胖',
+            color: '#FF1200'
+        };
+    }
+    return status;
+}
+
+// 渲染畫面
+function render() {
+    let str = '';
+    data.forEach(function(item) {
+        str += `
+        <li class="card d-flex flex-column align-items-center py-6 mb-1" style="color:${item.color}; border-color:${item.color}">
+            <h3 class="font-size-md font-weight-medium mb-3">${item.status}</h3>
+            <ul>
+                <li>BMI ${item.bmi}</li>
+                <li>height：${item.height}cm</li>
+                <li>weight：${item.weight}kg</li>
+            </ul>
+        </li>
+        `
+    })
+    list.innerHTML = str;
+}
